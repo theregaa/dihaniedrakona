2ac2181eb0c3299b20440b178e4f6f1b69ed51f9
theregaa <therega@yandex.com>
2026-09-06 04:57:18 +0300

    1

--- next.config.ts
+++
@@ -1,3 +0,0 @@
-import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { reactStrictMode: true, allowedDevOrigins: ['192.168.0.122'], devIndicators: false };
-export default nextConfig;
